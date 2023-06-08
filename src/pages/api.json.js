5a4cd637bb755c0b875d83290d49197ff6742994
hashrock <hashrock@users.noreply.github.com>
2023-06-09 01:45:27 +0900

    update api endpoint

--- src/pages/api.json.js
+++ src/pages/api.json.js
@@ -1,6 +1,9 @@
 export async function get({ params }) {
+  const dataStr = new Date().toISOString();
+
   return new Response(JSON.stringify({
-    message: "Hello from Astro!"
+    message: "Hello from Astro!",
+    now: dataStr
   }), {
     status: 200,
     headers: {
